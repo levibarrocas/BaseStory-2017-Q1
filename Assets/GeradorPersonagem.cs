c70dc56543d2fa753354a6e6777ca8a7a6b6cbaf
Levi Barrocas <levi_barrocas@hotmail.com>
2017-05-10 13:23:24 -0300

    Initial Build
    No art,no UI,just some basic code to start the development

---
+++ Assets/GeradorPersonagem.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GeradorPersonagem : MonoBehaviour {
+    [SerializeField]
+    string[] NomesMasculino;
+    [SerializeField]
+    string[] NomesFeminino;
+    [SerializeField]
+    string[] Sobrenomes;
+	
+	
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	
+	Personagem CriarPersonagemAleatorio(){
+		int R1 = Random.Range(0,1)
+		Personagem PER;
+		if(R1 == 0 {
+			R1.Sexo = true;
+			int R2 = Random.Range(0,NomesMasculino.Lenght);
+			int R3 = Random.Range(0,Sobrenomes.Lenght);
+			PER.NomeP = NomesMasculino[R2];
+			PER.Sobrenome = Sobrenomes[R3];
+		} else {
+			R1.Sexo = false;
+			int R2 = Random.Range(0,NomesFeminino.Lenght);
+			int R3 = Random.Range(0,Sobrenomes.Lenght);
+			PER.NomeP = NomesFeminino[R2];
+			PER.Sobrenome = Sobrenomes[R3];
+		}
+		
+		
+		
+	}
+}
---
+++ Assets/Personagem.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Personagem : MonoBehaviour {
+
+    public string NomeP;
+	public string Sobrenome;
+
+    // Pesquisa em Campo
+    public float Habilidade1;
+    // Pesquisa Teorica
+    public float Habilidade2;
+    // Marketing
+    public float Habilidade3;
+    // Lider
+    public float Habilidade4;
+
+    public float Salario;
+
+    public float Level;
+
+    public float Satisfacao;
+
+	
+	public string ClasseEspecial;
+	
+	// True para homem e false para mulher
+	public bool Sexo;
+	
+	public int CorDoCabelo;
+	public int CorDaPele;
+	public int Camisa;
+	public int Calca;
+	
+	string Nome(bool Sobrenome){
+		if(Sobrenome){
+			return NomeP + " " + Sobrenome;
+		} else {
+			return NomeP;
+		}
+	}
+	
+	
+	
+	
+}
